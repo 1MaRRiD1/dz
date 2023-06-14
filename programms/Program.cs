9acf5fc8961d6dd41406b2cf5f1c4cc6f47bfe2e
Maksim <norten1156@gmail.com>
2023-06-14 13:22:27 +0300

    решил все 3 задачи

--- programms/Program.cs
+++ programms/Program.cs
@@ -15,31 +15,49 @@ else
 */
 
 //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-/*int number = ReadInt("Введите число: ");
-int count = number.ToString().Length;
-Console.Write(MakeArray(number, count));
-int ReadInt(string message)
+/*int Prompt(string msg)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
 }
-int MakeArray(int a, int b)
+int number = Prompt("Введите число");
+
+int fnumber(int number)
 {
-    int result = 0;
-    if (b < 3)
-    {
-        Console.Write("Третьей цифры нет, держи: ");
-    }
-    else
-    {
-        int c = 1;
-        for (int i = b; i > 3; i--)
-        {
-            c = c * 10;
-        }
+while (number > 999)
+{
+number /= 10;
+}
+return number % 10;
+}
 
-        result = (a / c) % 10;
-    }
-    return result;
-}*/
+bool check(int number)
+{
+if (number < 100)
+return false;
+else return true;
+}
 
+if (check(number) != true)
+System.Console.WriteLine("Третьей цифры нет");
+else
+System.Console.WriteLine($"Третья цифра числа {number} является {fnumber(number)}");*/
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*int Prompt(string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+string day = "Это не день недели";
+string check(int number)
+{
+if (number >= 6 && number < 8) day = "Это выходной";
+if (number >= 1 && number < 6) day = "Это будний день";
+return day;
+}
+System.Console.WriteLine(check(number));
+*/
